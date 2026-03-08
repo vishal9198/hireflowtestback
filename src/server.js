2cdc7be88b231105645297ae2a7fb78866c9f5c7
vishal9198 <vkgupta4558@gmail.com>
2026-03-08 18:47:10 +0530

    fix clerk auth middleware

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { inngest, functions } from "./lib/inngest.js";
 import { serve } from "inngest/express";
-import { clerkMiddleware } from "@clerk/express";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoute.js";
 const app = express();
@@ -31,9 +31,8 @@ app.use(clerkMiddleware()); //this adds auth field to req object//you can call r
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use("/api/chat", chatRoutes);
-
-app.use("/api/sessions", sessionRoutes);
+app.use("/api/chat", requireAuth(), chatRoutes);
+app.use("/api/sessions", requireAuth(), sessionRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({

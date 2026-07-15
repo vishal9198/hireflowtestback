5517ebee979b7932fb52a7c5be23f02ad4925eba
vishal9198 <vkgupta4558@gmail.com>
2026-07-15 20:53:42 +0530

    add company db for saas

---
+++ src/models/Company.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const companyScheam = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    logo: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Company = mongoose.model("Company", companyScheam);
+
+export default Company;

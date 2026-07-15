0ca2b8bd15082e7d66ef8f10360829d470911b97
vishal9198 <vkgupta4558@gmail.com>
2026-07-15 20:59:36 +0530

    add company detail in user schema

--- src/models/User.js
+++ src/models/User.js
@@ -20,8 +20,26 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    //new role for rbac
+    role: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    //which company this user belongs to
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      default: null,
+    },
+
+    //whether account is active
+    isActive: {
+      type: String,
+      default: true,
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 const User = mongoose.model("User", userSchema);
 export default User;
